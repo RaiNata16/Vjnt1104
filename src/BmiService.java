6585a19e2d78e8c266430068a88b4bbf15e1a8a6
toncha <nata.toncha@mail.ru>
2023-04-17 16:39:43 +0300

    Nt11java

---
+++ src/BmiService.java
@@ -0,0 +1,10 @@
+public class BmiService {
+    public double calculate(double height, double mass) {
+
+
+            double index = mass / height / height;
+
+
+        return index;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int bmi;
+        double mass = 98; // вес телa в кг
+        double height = 1.87 ; // рост в метрaх
+        BmiService service = new BmiService();
+
+        double indexMy = service.calculate(height, mass);
+        int index = (int) indexMy;
+        System.out.println("Индекс массы тела" +  index);
+    }
+}
